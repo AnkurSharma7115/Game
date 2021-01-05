581f3b4cb71e3068edcded84ab865f6aa3454b3e
Ankur Sharma <sharma18ankur@gmail.com>
2021-01-05 12:11:26 +0530

    Adding Attack Button Functionality

--- Monster_Killer/assets/scripts/vendor.js
+++ Monster_Killer/assets/scripts/vendor.js
@@ -8,26 +8,27 @@ const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
 
 function adjustHealthBars(maxLife) {
-  monsterHealthBar.max = maxLife;
-  monsterHealthBar.value = maxLife;
-  playerHealthBar.max = maxLife;
-  playerHealthBar.value = maxLife;
+  monsterHealthBar.max = maxLife;    //for progress bar
+  monsterHealthBar.value = maxLife;   // for progress bar actual value
+  
+  playerHealthBar.max = maxLife;  //for progress bar
+  playerHealthBar.value = maxLife;  // for progress bar actual value
 }
 
 function dealMonsterDamage(damage) {
   const dealtDamage = Math.random() * damage;
-  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;  //converting to Int and updating value
   return dealtDamage;
 }
 
 function dealPlayerDamage(damage) {
   const dealtDamage = Math.random() * damage;
-  playerHealthBar.value = +playerHealthBar.value - dealtDamage;
+  playerHealthBar.value = +playerHealthBar.value - dealtDamage;   //converting to Int and updating value
   return dealtDamage;
 }
 
 function increasePlayerHealth(healValue) {
-  playerHealthBar.value = +playerHealthBar.value + healValue;
+  playerHealthBar.value = +playerHealthBar.value + healValue; //converting to Int and updating value
 }
 
 function resetGame(value) {
@@ -42,3 +43,34 @@ function removeBonusLife() {
 function setPlayerHealth(health) {
   playerHealthBar.value = health;
 }
+
+// JavaScript Code for Executing Game Fnctions
+
+const PLAYER_ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 12;
+
+
+let chosenMaxLife = 100;
+
+let currentMosnterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler(){
+  const monsterDamage = dealMonsterDamage(PLAYER_ATTACK_VALUE);
+  currentMosnterHealth = currentMosnterHealth - monsterDamage;
+  
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);  
+  currentPlayerHealth = currentPlayerHealth - playerDamage;
+
+  if(currentMosnterHealth <= 0 && currentPlayerHealth > 0){
+    alert("Hurray!!! You've WON!!!");
+  }else if(currentPlayerHealth <= 0 && currentMosnterHealth > 0){
+    alert("OOPS!!! You LOOSE!!!");
+  }else if(currentMosnterHealth <= 0 && currentPlayerHealth <= 0){
+    alert("It's a DRAW MATCH!!");
+  }
+}
+
+attackBtn.addEventListener("click", attackHandler);
